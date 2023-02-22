8ae60260e40bc1e85f1bff049799d21352a3d438
KUBIXQAZ <kubajozwik20027@gmail.com>
2023-02-22 21:35:09 +0100

    unfinished_prototype

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function change_from(menu) {
-    if(menu == 0) {
-        document.getElementById("sign_up_form").style.visibility = "false";
-        document.getElementById("sign_in_from").style.visibility = "true";
+    if (menu == 0) {
+        var sign_in_from = document.getElementById("sign_in_form");
+        var sign_up_form = document.getElementById("sign_up_form");
+
+        sign_in_from.style.visibility = "visible";
+        sign_up_form.style.visibility = "hidden";
     }
     if (menu == 1) {
-        document.getElementById("sign_in_from").style.visibility = "false";
-        document.getElementById("sign_up_form").style.visibility = "true";
+        var sign_in_from = document.getElementById("sign_in_form");
+        var sign_up_form = document.getElementById("sign_up_form");
+
+        sign_in_from.style.visibility = "hidden";
+        sign_up_form.style.visibility = "visible";
     }
 }
